ee93462942165a08895425a867f637ad9a398f59
Vishal-V <vishal114186@gmail.com>
2019-02-23 18:28:25 +0530

    Added all styles and working app

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template, url_for, redirect
 app = Flask("__app__")
 app.config['SECRET_KEY'] = 'a551d32359baf371b9095f28d45347c8b8621830'
 
-@app.route('/home', methods=['GET, POST'])
+@app.route('/')
 def home():
-	return render_template('index.html', title='SIH Home')
+	return render_template('index.html', title='SIH 2019')
 
 @app.route('/rooftop')
 def rooftop():
 	return render_template('roof.html', title='Rooftop Detection')
 
-app.run(debug=True)
+@app.route('/references')
+def references():
+	return render_template('references.html')
+
+app.run(debug=True, port=5001)
