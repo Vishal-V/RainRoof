3f988311f5d3a2cdd42e606649b623ea148be791
Vishal-V <vishal114186@gmail.com>
2019-03-01 18:08:40 +0530

    Compatible co-ordibates

--- app.py
+++ app.py
@@ -1,4 +1,63 @@
 from flask import Flask, render_template, url_for, redirect
+import json
+import urllib
+
+def elevation(request):
+	apikey = "AIzaSyDv9C5WnFwlmPtZWMtH6EqfMhSwJrlCcD0"
+	url = "https://maps.googleapis.com/maps/api/elevation/json"
+	request = urllib.request.urlopen(url+"?locations="+str(request)+"&key="+apikey)
+	try:
+		results = json.load(request).get('results')
+		if 0 < len(results):
+			mat={}
+			for i in range(0,len(results)):	    
+				elevation = results[i].get('elevation')
+				location=results[i].get('location')
+				loclat=[]
+				loclat.append(location['lat'])
+				loclat.append(location['lng'])
+				loc=tuple(loclat)
+				if elevation not in mat:
+					mat[elevation]=[]
+				mat[elevation].append(loc)
+				# ELEVATION
+			return mat
+		else:
+			print ('HTTP GET Request failed.')
+	except ValueError as e:
+		print ('JSON decode failed: '+str(request))
+def postion(lat1,lon1,lat2,lon2):
+  if(lat1>lat2):  # swaping cordinates to get range of latitude and longitude 
+    temp=lat1
+    lat1=lat2
+    lat2=temp
+  if(lon1>lon2):
+    temp=lon1
+    lon1=lon2
+    lon2=temp
+  res=''   #initializing string with null value
+  i=0.0
+  i=lat1	#itration variable for varying latitude
+  while i<lat2:
+    j=0.0
+    j=lon1	#iteration variable for varying longitude
+    while j<lon2:
+      res=res+(str(i))	# adding current latitude to string
+      res=res+','	# separator for latitude and longitude
+      if((i+0.0001)>=lat2 and (j+0.0001)>=lon2):	#cheacking wheather the coordinate is last one
+        res=res+(str(j))	# last coordinate need not to have '|' as its the last one
+      else:
+        res=res+(str(j))	#else we need to saperate the coordinates with '|'
+        res=res+'|'	#adding '|' after one coordinate is entered
+      j=j+0.0001		#increasing longitude by 10 meters
+    i=i+0.0001		#increasing latitude by 10 meters
+  result=elevation(res)		#calling elevaton function to get elevation data
+  rest={}			#dictonary 
+  for key in sorted(result.keys()):	#getting elevation in sorted order
+    rest[key]=result[key]	
+  print(rest)		#getting elevated data in increasing order
+
+# postion(13.00011,77.00011,13.0011,77.00111)
 
 app = Flask("__app__")
 app.config['SECRET_KEY'] = 'a551d32359baf371b9095f28d45347c8b8621830'
---
+++ static/js/calc.js
@@ -0,0 +1,61 @@
+
+	$('#calculate').click(function(e){
+
+		var batteryCapacity = 12; // kWh
+		var batteryPrice = 323457; // USD
+		var sunPerDay = 4; // h
+		var panelGeneration = 0.15 // kWh
+		var generatePrice = 0.3 // USD/W
+
+		var bdInKw = 10; // get from location
+		var electricityPrice = 0.12 // USD/kWh
+
+		var fans = $('#householdBDs').val();
+		var ac = $('#householdBDs1').val();
+		var cfl = $('#householdBDs2').val();
+		var pc = $('#householdBDs3').val();
+
+
+		var consumefan = (fans) * bdInKw * 0.15;
+		var consumeac = (ac) * bdInKw * 0.85;
+		var consumecfl = (cfl) * bdInKw * 0.20;
+		var consumepc = (pc) * bdInKw * 0.45;
+
+		var consumetot = consumefan + consumepc + consumecfl + consumeac;
+
+		var batteries = Math.floor(consumetot / (batteryCapacity));
+
+		$('#batteryPacks').val(batteries);
+		val = batteries;
+
+		var generateCapacity = consumetot / sunPerDay;
+
+		var panelSize = generateCapacity / panelGeneration
+		$('#panelSize').val(panelSize);
+
+		var investment = batteries * batteryPrice + generateCapacity  * generatePrice;
+
+		$('#investment').val(investment);
+
+		var returnOfInvestment = Math.floor(investment / electricityPrice / 24 / 365);
+		returnOfInvestment = Math.round(returnOfInvestment / 77);
+
+		$('#returnOfInvestment').val(returnOfInvestment + ' year(s)');
+	});
+
+	btn = document.getElementById("tips");
+	btn.addEventListener("click", function(e){
+
+		if(val <= 2){
+
+			alert("Low power usage! You can use another device.");
+		}
+		else if(val === 3 || val === 4){
+			alert("Mid-range power usage for a humble home!!");
+		}
+		else if(val > 4) {
+			alert("High power usage! Please consume lesser power and save the environment!");
+		}
+
+	});
+
