a58c90cea046fa0bf9e12e36a78ee7d1399021b5
Vishal-V <vishal114186@gmail.com>
2019-03-02 11:19:17 +0530

    Watershed results

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ SIH submission for GoldMan Sachs' problem statement at NIT Calicut
   
 ### Planned components:
 1. Rooftop Detection (`Watershed Algorithm` + `Canny Edge Detection`)
-2. Depth Estination + Volume Calculation (Of the water collected)
+2. Depth Estimation + Volume Calculation (Of the water collected)
 3. Rainfall Prediction
 4. Calculator (Cost of installation + Break-Even analysis)
 5. Tax Benefits
---
+++ static/js/canny.js
@@ -0,0 +1,28 @@
+let uint8_lena = lenna_256x256;
+let uint16_lena = lenna_256x256.map ( (px) => px * 256);
+let float_lena = lenna_256x256.map( (px) => px/256 );
+
+//let type = 'uint16';
+//let pix = uint16_lena;
+let W = 256;
+let H = 256;
+//128-105 , 300-246 , 512-420 , 1024-840 , 2048-1679, 4096-3360
+let img = new T.Image('uint8',W,H);
+img.setPixels(new Uint8Array(uint8_lena));
+let gpuEnv = gpu.getGraphicsContext("preview");
+gpuDisplay(img.getRaster(),gpuEnv);
+
+let img1 = new T.Image('uint8',W,H);
+img1.setPixels(new Uint8Array(uint8_lena));
+let gpuEnv1 = gpu.getGraphicsContext("preview1");
+gpuEdgeCanny(50.0,100.0)(img1.getRaster(),gpuEnv1);
+
+let img2 = new T.Image('uint16',W,H);
+img2.setPixels(new Uint16Array(uint16_lena));
+let gpuEnv2 = gpu.getGraphicsContext("preview2");
+gpuEdgeCanny(50.0,100.0)(img2.getRaster(),gpuEnv2);
+
+let img3 = new T.Image('float32',W,H);
+img3.setPixels(new Float32Array(float_lena));
+let gpuEnv3 = gpu.getGraphicsContext("preview3");
+gpuEdgeCanny(50.0,100.0)(img3.getRaster(),gpuEnv3);
